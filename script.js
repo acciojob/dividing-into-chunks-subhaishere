54a920c2fc2841de1c72e9b927405d9e42268e20
subhaishere <60627114+subhaishere@users.noreply.github.com>
2023-08-14 01:34:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
+const sum = (arr) => arr.reduce((a, b) => a + b, 0);
+
 const divide = (arr, n) => {
-  // Write your code here
+  let result = [];
+  let subarray = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    subarray.push(arr[i]);
+    if (sum(subarray) > n) {
+      result.push(subarray.slice(0, -1));
+      subarray = [arr[i]];
+    }
+  }
+
+  if (subarray.length > 0) {
+    result.push(subarray);
+  }
+
+  return result;
 };
 
 const n = prompt("Enter n: ");
